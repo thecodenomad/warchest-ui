480a78efba6156152eb05d7f801996bec6a65e88
codenomad <ray.gomez@gmail.com>
2021-11-07 20:19:46 -0700

    Add a little more flare.

--- src/components/WarchestWallet.vue
+++ src/components/WarchestWallet.vue
@@ -2,42 +2,67 @@
   <div class="wc-wallet">
     <h2>Warchest!</h2>
     <v-divider></v-divider>
-    <div v-if="errored || wc_coins.length == 0">
-      <img src="@/assets/sad-panda.png">
-      <h3 class="sad-panda-text">Sad day. No coins!</h3>
+    <div v-if="!errored && Object.keys(wc_coins).length === 0">
+      <img class="sad-panda-img" src="@/assets/sad-panda.png" alt="">
+      <h3 class="sad-panda-text">Sad day. No coins found.</h3>
     </div>
-    <div v-else-if="wc_coins.length > 0">
-      <v-card v-for="(coin, symbol) in wc_coins" :key="symbol" class="mx-auto wc-card" outlined>
-        <v-img
-            src=""
-            class="white--text align-end"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-        >
-          <v-card-title v-text="symbol"></v-card-title>
-          <v-card-subtitle>${{ coin.amount*coin.rates["USD"]}}</v-card-subtitle>
-        </v-img>
-
-        <v-card-text>
-          <v-btn class="wc-button">
-            <v-icon>mdi-fire</v-icon>
-          </v-btn>
-
-          <v-btn class="wc-button">
-            <v-icon>mdi-chart-line</v-icon>
-            Stats
-          </v-btn>
-
-          <v-btn class="wc-button">
-            <v-icon>mdi-format-list-bulleted</v-icon>
-            Transactions
-          </v-btn>
-        </v-card-text>
-      </v-card>
+    <div v-else-if="errored">
+      <img src="@/assets/sad-panda.png" alt="">
+      <h3 class="sad-panda-text">Sad day. Config issue. U haz API kayz?</h3>
     </div>
     <div v-else>
-      <img src="@/assets/sad-panda.png">
-      <h3 class="sad-panda-text">Sad day. Config issue. U haz API kayz?</h3>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card v-for="(coin, symbol) in wc_coins" :key="symbol" class="wc-card mx-auto">
+              <v-card-text>
+                <v-row align="center"><v-col><h2>{{ symbol }}</h2></v-col></v-row>
+                <v-row align="center">
+                  <v-col
+                    class="text-h2"
+                    cols="8"
+                  >
+                    <v-row>
+                      <v-col>
+                        <v-chip v-if="coin.profit > 0" class="ma-2 profitable wc-stats">
+                          <v-icon large left>mdi-arrow-top-right</v-icon> {{ formatCurrency(coin.profit) }}
+                        </v-chip>
+                        <v-chip v-else class="ma-2 red wc-stats">
+                          <v-icon large>mdi-arrow-bottom-right</v-icon>{{ formatCurrency(coin.profit) }}
+                        </v-chip>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col>
+                        <v-chip class="ma-2 wc-cost">
+                          <v-icon large>mdi-currency-usd</v-icon>Investment: {{ formatCurrency(coin.profit) }}
+                        </v-chip>
+                      </v-col>
+                    </v-row>
+
+                  </v-col>
+                  <v-col cols="4">
+                    <v-img class="wc-coin-img" :src="coin.image_uri" width="125px"></v-img>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="wc-button">
+                  <v-icon>mdi-fire</v-icon>
+                </v-btn>
+                <v-btn class="wc-button">
+                  <v-icon>mdi-chart-line</v-icon>
+                  Stats
+                </v-btn>
+                <v-btn class="wc-button">
+                  <v-icon>mdi-format-list-bulleted</v-icon>
+                  Transactions
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </div>
   </div>
 </template>
@@ -61,6 +86,13 @@ export default {
     } catch (e) {
       this.errored = true
       console.log("Failed! This is why: "+e)
+      this.wc_coins = {}
+    }
+  },
+  methods:{
+    formatCurrency: function (currency) {
+      currency = currency.toFixed(2);
+      return currency
     }
   }
 }
@@ -72,6 +104,10 @@ Reference: https://www.vuemastery.com/courses/real-world-vue-js/single-file-vue-
 Lesson 6 - https://github.com/Code-Pop/real-world-vue
 -->
 <style scoped>
+h3 {
+  padding-top: 25px;
+  padding-bottom: 25px;
+}
 .wc-card {
   padding: 20px;
   margin-bottom: 5px;
@@ -86,32 +122,35 @@ Lesson 6 - https://github.com/Code-Pop/real-world-vue
   margin: 0;
 }
 
+.wc-coin-img {
+
+}
+
 .wc-button {
   margin: 5px;
 }
 
-.sad-panda {
+.sad-panda-img {
   margin-top: 20px;
   margin-bottom: 20px;
 }
 
-.wc-link {
-  color: black;
-  text-decoration: none;
-  font-weight: 100;
+.wc-coin-stats {
+  padding-top: 15px;
+  font-size: 20px;
 }
-.red-alert {
-  color: darkred;
+
+.profitable i.v-icon.v-icon {
+  color: green;
 }
-.profitable {
-  color: darkgreen;
-  text-blink: 1s;
-  font-weight: bolder;
+
+.red i.v-icon.v-icon {
+  color: red;
 }
-.wc-coins {
-  padding-right: 10px;
+
+.wc-cost i.v-icon.v-icon {
+  color: darkcyan;
+  font-size: 14px;
 }
-.wc-card-stats {
-  text-align: left;
-}
+
 </style>
